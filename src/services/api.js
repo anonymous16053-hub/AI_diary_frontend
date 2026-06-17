c7c2d936af4e898de386b79e9072a38adb531668
anonymous16053-hub <anonymous16053@gmail.com>
2026-06-17 22:09:18 +0530

    Update API baseURL to use environment variable

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  // baseURL: import.meta.env.VITE_API_URL,
-  baseURL: "http://127.0.0.1:5000/",
+  baseURL: import.meta.env.VITE_API_URL,
+  // baseURL: "http://127.0.0.1:5000/",
 });
 
 export default API;
